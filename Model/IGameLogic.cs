d3367397d4cdc355791d9ee726b2c9227973690c
Silvirua <silvirua@naver.com>
2026-04-09 16:35:26 +0900

    Update IGameLogic.cs

--- Model/IGameLogic.cs
+++ Model/IGameLogic.cs
@@ -1,4 +1,11 @@
 namespace Game_analysis_MVPPattern.Model{
+  public interface IGameLogic{
+    // player 재화 변경 이벤트
+    public event Action<int> OnPlayerGemChanged;
 
+    // 가챠 결과 이벤트
+    public event Action<string> OnDrawResultLog;
+
+    // 
 
 }
