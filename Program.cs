e9d9ceea0eaad2e595547cd171ca8ea24a06c410
Silvirua <silvirua@naver.com>
2026-04-10 18:30:35 +0900

    Update Program.cs
    메인 코드 추가

--- Program.cs
+++ Program.cs
@@ -2,9 +2,15 @@ using Game_analysis_MVPPattern.Model;
 using Game_analysis_MVPPattern.Presenter;
 using Game_analysis_MVPPattern.View;
 
-class Program{
-  static void Main(string[] args){
+class Program
+{
+    static void Main(string[] args)
+    {
+        var model = new GameLogic();
+        var view = new SystemView();
+        var presenter = new GamePresenter(view, model);
 
-
-  }
+        Console.WriteLine(" == MVP Pattern 뽑기 시스템 구현 == \n");
+        view.StartGameLoop();
+    }
 }
